dcc97e027972fc0163ecddbddc06d1a840cdca4f
janthony25 <junanthony.business@gmail.com>
2024-02-06 06:50:55 +1300

    Add project files.

---
+++ Getter-Setter2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Getter & Setters - Practice 
+            Motorcycle motor1 = new Motorcycle(130);
+            Console.WriteLine(motor1.Motor);
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class Motorcycle
+    {
+        private int motor;
+
+        public Motorcycle(int motor)
+        {
+            Motor = motor;
+        }
+
+        public int Motor
+        {
+            get { return motor; }
+            set
+            {
+                if (value > 120)
+                {
+                    motor = 120;
+                }
+                else
+                {
+                    motor = value;
+                }
+            }
+
+        }
+    }
+
+}
